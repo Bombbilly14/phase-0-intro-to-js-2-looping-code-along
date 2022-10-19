e7b5de653dbebe9e924f0c7ffbebe9f6ba42355a
William German <william.j.german2@gmail.com>
2022-10-19 13:05:31 -0700

    commit tree 2cea22c8dda123a1a75758e446101a1bfded4f45

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+   console.log(name)
+   console.log(event)
+   const newCard=[]
+   for (let i=0; i < name.length; i++){
+    console.log(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    newCard.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+   }
+return newCard
+}
+function countDown(startingNumber) {
+
+    while(startingNumber >= 0){
+        console.log(startingNumber)
+        startingNumber--   
+    }
+
+}
